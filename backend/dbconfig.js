50b27e4ec845e92cb2fcd89c260287739cd3bbf9
nancydahiya23 <nancydahiya2378@gmail.com>
2025-11-22 13:29:41 +0530

    Updated dbconfig.js file

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const url="mongodb+srv://nancydb:DahiyaN@cluster0.ucpgrgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = process.env.MONGO_URL;
 const dbName="node-project";
 export const collectionName = "todo";
 const client = new MongoClient(url)
@@ -8,3 +8,4 @@ export const connection=async()=>{
     const connect = await client.connect();
     return await connect.db(dbName)
 }
+
